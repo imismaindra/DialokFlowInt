3c9fba75d3e4df0e954b3d78eae52f9cab86b071
Ismaindra <imismaindra@gmail.com>
2022-01-04 21:07:15 +0700

    first commit

---
+++ Backend/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class ChatBot(models.Model):
+    order = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.order
---
+++ Backend/api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ChatBot
+from .serializers import ChatSerializer
+
+class ChatViewSet(viewsets.ModelViewSet):
+    queryset = ChatBot.objects.all()
+    serializer_class = ChatSerializer
